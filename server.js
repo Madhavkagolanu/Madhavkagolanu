d2b4ec38881e3ebc4f58d334cf3a85648c39b2d4
Madhavkagolanu <chandra8madhav@gmail.com>
2022-12-13 00:45:04 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+var cors = require('cors');
+const stripe = require('stripe')('sk_test_51LY1QBSIqQO61hDB1N5xIHmXGhlBCe4vcJ3XUMod7MDiwTauydAjpETHWKh2iBdJiGkiQh8C3xwt0Ya2hBuPEFNi00kEhiboPC');
+
+const app = express();
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.json());
+app.get('/',(req,res)=>{
+    res.json({'msg':'HAII'})
+})
+app.post("/checkout", async (req, res) => {
+   
+    console.log(req.body);
+    const items = req.body.items;
+    let lineItems = [];
+    items.forEach((item)=> {
+        lineItems.push(
+            {
+                price: item.id,
+                quantity: item.quantity
+            }
+        )
+    });
+
+    const session = await stripe.checkout.sessions.create({
+        line_items: lineItems,
+        mode: 'payment',
+        success_url: "http://localhost:3000/success",
+        cancel_url: "http://localhost:3000/cancel"
+    });
+ 
+    res.send(JSON.stringify({
+        url: session.url
+    }));
+});
+
+app.listen(4000, () => console.log("Listening on port 4000!"));
